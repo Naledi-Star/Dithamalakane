7919da2b6963239f50ee17db7589e401ad6b004f
Naledi-Star <111863176+Naledi-Star@users.noreply.github.com>
2024-05-29 11:31:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,74 +1,86 @@
-'use strict';
+let dithamalkn = [
+    {
+        thamalkn:`ka itaya mpipi, mpipi a thoka lebadi`,
+        thaloso: `metsi`
+    },
+    {
+        thamalkn:`ka itaya mpipi, mpipi a tswa lebadi`,
+        thaloso: `seretse`
+    },
+    {
+        thamalkn:`monna yo moleele, rahiho`,
+        thaloso: `mosi`
+    },
+    {
+        thamalkn:`O seka wa mpona ke le kana, ke monkane wa rrago`,
+        thaloso: `sesana`
+    },
+    {
+        thamalkn:`banna ba ditedu tse di tshweu ko tshimong`,
+        thaloso: `mmidi`
+    },
+    {
+        thamalkn:`Sa re nyedi, sa re tseke`,
+        thaloso: `Legadima`
+    },
+    {
+        thamalkn:`Monna yo moleele, o tshologa ditete`,
+        thaloso: `Kerese`
+    },
+];
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
+const quoteBtn = document.getElementById('quote-btn');
+const quote = document.querySelector('.quote-area');
+const thaloso = document.querySelector('.thaloso');
+const thamalkn = document.querySelector('.thamalkn')
+const description = document.getElementById('description'); 
 
-let score = 20;
 
-let highScore = 0;
 
-const displayMessage = function (message) {
-	document.querySelector('.message').textContent = message;
-};
+quoteBtn.addEventListener('click', ()=>{
+    let random =  Math.floor(Math.random() * dithamalkn.length);
 
-document.querySelector('.check').addEventListener('click', function () {
-	const guess = Number(document.querySelector('.guess').value);
+    thamalkn.innerHTML = dithamalkn[random].thamalkn;
+    thaloso.innerHTML = dithamalkn[random].thaloso;
+    thaloso.style.display = 'none'
 
-	if (!guess) {
-		// document.querySelector('.message').textContent = '🚩🚩 No number!!!';
-		displayMessage('🚩🚩 No number!!!');
-	} else if (guess === secretNumber) {
-		// document.querySelector('.message').textContent = 'Correct Number💕';
-		displayMessage('Correct Number💕');
-		document.querySelector('.number').textContent = secretNumber;
-		document.querySelector('body').style.backgroundColor = '#60b347';
-		document.querySelector('.number').style.width = '30rem';
+    
+    
+})
 
-		if (score > highScore) {
-			highScore = score;
-			document.querySelector('.highscore').textContent = highScore;
-		}
-	} else if (guess !== secretNumber) {
-		if (score > 1) {
-			// document.querySelector('.message').textContent = guess > secretNumber ? 'Too high📈' : 'Too low📉';
-			displayMessage(guess > secretNumber ? 'Too high📈' : 'Too low📉');
-			score--;
-			document.querySelector('.score').textContent = score;
-		} else {
-			// document.querySelector('.message').textContent = 'You lost the game😢';
-			displayMessage('You lost the game😢');
+// description.addEventListener('click', ()=>{
+//     btn.style.display = 'none';
+   
+// })
 
-			document.querySelector('.score').textContent = 0;
-		}
-	}
-	// } else if (guess > secretNumber) {
-	//   if (score > 1) {
-	//     document.querySelector('.message').textContent = 'Too high📈';
-	//     score--;
-	//     document.querySelector('.score').textContent = score;
-	//   } else {
-	//     document.querySelector('.message').textContent = 'You lost the game😢';
-	//     document.querySelector('.score').textContent = 0;
-	//   }
-	// } else if (guess < secretNumber) {
-	//   if (score > 1) {
-	//     document.querySelector('.message').textContent = 'Too low📉';
-	//     score--;
-	//     document.querySelector('.score').textContent = score;
-	//   } else {
-	//     document.querySelector('.message').textContent = 'You lost the game😢';
-	//     document.querySelector('.score').textContent = 0;
-	//   }
-	// }
-});
+ 
 
-document.querySelector('.again').addEventListener('click', function () {
-	score = 20;
-	secretNumber = Math.trunc(Math.random() * 20) + 1;
-	document.querySelector('.guess').value = '';
-	// document.querySelector('.message').textContent = 'Start guessing...';
-	displayMessage('Start guessing...');
-	document.querySelector('.number').textContent = '?';
-	document.querySelector('.score').textContent = score;
-	document.querySelector('body').style.backgroundColor = '#222';
-	document.querySelector('.number').style.width = '15rem';
-});
+let display = 0;
+
+description.addEventListener('click', ()=>{
+    console.log('clicked')
+    if (display==1){
+        thaloso.style.display = 'block';
+        display = 0;
+    }
+    else{
+        thaloso.style.display = 'none';
+        display = 1;
+    }
+    
+})
+
+// function hideShow(){
+//     if (display==1){
+//         thaloso.style.display = 'block';
+//         display = 0;
+//     }
+//     else{
+//         thaloso.style.display = 'none';
+//         display = 1;
+//     }
+    
+
+// }
+
+
